3feb176841891b9ddeada0ca73c252e77cf6f265
KeisukeHamatani <s1190077@gmail.com>
2018-06-03 00:40:31 +0900

    設定ファイルをバックアップ

---
+++ elpa/go-autocomplete-20170907/go-autocomplete-pkg.el
@@ -0,0 +1,1 @@
+(define-package "go-autocomplete" "20170907" "auto-complete-mode backend for go-mode" (quote ((auto-complete "1.4.0"))))
---
+++ shellenv.el
@@ -0,0 +1,1 @@
+(setenv "PATH" "/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin:/opt/local/bin:/Users/hamakei/.go/bin")
